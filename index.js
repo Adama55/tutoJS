533c173bdb10f43aed1ac4655b7f5e113ea23b4c
Adama55 <mafahfof55@gmail.com>
2022-09-15 03:52:10 +0200

    mode affichage alert, consol et inner

--- index.js
+++ index.js
@@ -1,29 +1,9 @@
 
+let prenom ="je suis Adma";
+let age = 29;
+let age2 = '29';
 
-let integerCalcule = 1+ 2;
-console.log(integerCalcule);
-
-
-
-
-
-
-
-/*let cours ='jio ipoj iofjc eip jg ivj';
-
-console.log(cours + ' // avant MAJ')
-cours ='je change'
-console.log(cours + ' // avant MAJ')
-// ce je retien
-//console.log(variable) affiche le contenue de la variable dans le console 
-
-function classique (){
-    console.log('je suis une declation de fonction');
-}
-
-const fonctionFlecher = (argument) => {
-    console.log('je suis fonction declarer en mode flecher' + argument);
-}
-
-classique();
-fonctionFlecher(' merci!'); */
+document.getElementById('p1').innerHTML ='type prenom :' + typeof prenom;
+document.getElementById('p2').innerHTML = 'type de age :' + typeof age;
+alert('type age2 :' + typeof age2);
+console.log('type age2 :' + typeof age2)

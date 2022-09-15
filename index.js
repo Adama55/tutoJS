1fd24e9b85a672ead366bb7232bcd752f1a2d6bd
Adama55 <mafahfof55@gmail.com>
2022-09-15 02:08:59 +0200

    premier commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+let integerCalcule = 1+ 2;
+console.log(integerCalcule);
+
+
+
+
+
+
+
+/*let cours ='jio ipoj iofjc eip jg ivj';
+
+console.log(cours + ' // avant MAJ')
+cours ='je change'
+console.log(cours + ' // avant MAJ')
+// ce je retien
+//console.log(variable) affiche le contenue de la variable dans le console 
+
+function classique (){
+    console.log('je suis une declation de fonction');
+}
+
+const fonctionFlecher = (argument) => {
+    console.log('je suis fonction declarer en mode flecher' + argument);
+}
+
+classique();
+fonctionFlecher(' merci!'); */

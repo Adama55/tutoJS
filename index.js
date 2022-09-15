871c7f534d92155cd9678d95c551c50a67c7c782
Adama55 <mafahfof55@gmail.com>
2022-09-15 14:54:04 +0200

    ex0 conversion de poids

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-function Utilisateur(a, b, c){
-    this.nom =a;
-    this.age =b;
-    this.mail =c;
-
-    this.bonjour = function(){
-        alert('bonjour, je suis ' + this.nom + ', ' + this.age +' ans ' )
+function convPoids(id, valeur){
+    if(id == "grammes"){
+        kilos.value = valeur /1000;
+        livres.value = valeur * 0.0022046;
+    }else if(id == "kilos"){
+        grammes.value = valeur * 1000;
+        livres.value = valeur * 2.2046;
+    }else if(id == "livres"){
+        grammes.value = valeur / 0.0022046;
+        kilos.value = valeur / 2.2046;
     }
 }
+
+let grammes = document.getElementById("grammes");
+let kilos = document.getElementById("kilos");
+let livres = document.getElementById("livres");
+
+grammes.addEventListener(
+    "input",function(){
+        convPoids(this.id, this.value);
+        });
+kilos.addEventListener(
+    "input",function(){
+        convPoids(this.id, this.value);
+        });
+livres.addEventListener(
+    "input",
+     function(){
+        convPoids(
+            this.id,
+            this.value);});

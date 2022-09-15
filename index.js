49b548c0366a278a7484e267de80fc1e329b43fe
Adama55 <mafahfof55@gmail.com>
2022-09-15 10:03:48 +0200

    notion classe, fonction

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+function Utilisateur(a, b, c){
+    this.nom =a;
+    this.age =b;
+    this.mail =c;
 
-let prenom ="je suis Adma";
-let age = 29;
-let age2 = '29';
-
-document.getElementById('p1').innerHTML ='type prenom :' + typeof prenom;
-document.getElementById('p2').innerHTML = 'type de age :' + typeof age;
-alert('type age2 :' + typeof age2);
-console.log('type age2 :' + typeof age2)
+    this.bonjour = function(){
+        alert('bonjour, je suis ' + this.nom + ', ' + this.age +' ans ' )
+    }
+}
